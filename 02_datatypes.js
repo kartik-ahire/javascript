5683d5e84c65fc59c230c0afbb18a9dc4e535ffe
kartik-ahire <119925465+kartik-ahire@users.noreply.github.com>
2023-10-16 12:48:29 +0530

    Datatypes in JS

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // can't execute on node, but on browser
+// code readability should be high
+
+// console.log(3+4)
+// console.log("Kartik")
+
+// Data types 
+let name = "Kartik"
+let age = 21
+let login = true
+
+// --------------------- Primitive data types -----------------------
+// number    => 1 to 2^53
+// bigInt    => 213153465461566
+// string    => ""
+// boolean   => true/false
+// null      => standalone value [empty value]
+// undefined => variable declared but value not defined
+// symbol    => unique
+
+// object
+
+console.log(typeof("kartik"));
+console.log(typeof([]));             // object
+console.log(typeof(null))            // object
+console.log(typeof(undefined))       // undefined
