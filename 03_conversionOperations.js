48de7fa4e55e812165b585d6c20427c8ad634b26
kartik-ahire <119925465+kartik-ahire@users.noreply.github.com>
2023-10-16 12:49:04 +0530

    Data type Conversion in JS

---
+++ 03_conversionOperations.js
@@ -0,0 +1,87 @@
+// Converting data types
+// ==================================
+
+// let score1 = 33.3
+// let score2 = "33abc"
+// let score3 = true
+// let score4 = null
+// let score5 = undefined
+// let score6 = 'a'
+
+// Converting to Number
+// ---------------------
+// num          = num
+// string       = NaN
+// boolean      = 0/1
+// null         = 0
+// undefined    = NaN
+
+// let c1 = Number(score1)
+// let c2 = Number(score2)
+// let c3 = Number(score3)
+// let c4 = Number(score4)
+// let c5 = Number(score5)
+// let c6 = Number(score6)
+// console.table([c1,typeof (c1),c2,typeof (c2),c3,typeof (c3),c4,typeof (c4),c5,typeof (c5),c6,typeof (c6)])
+
+// Converting to Boolean
+// ---------------------
+// 1    = true
+// 0    = false
+// ""   = false
+// "Ka" = true
+
+// let c1 = Boolean(score1)
+// let c2 = Boolean(score2)
+// let c3 = Boolean(score3)
+// let c4 = Boolean(score4)
+// let c5 = Boolean(score5)
+// let c6 = Boolean(score6)
+// console.table([c1,typeof (c1),c2,typeof (c2),c3,typeof (c3),c4,typeof (c4),c5,typeof (c5),c6,typeof (c6)])
+
+
+// Converting to String
+// ---------------------
+// null      = 'null'
+// undefined = 'undefined'
+// 22        = '22'
+
+// let c1 = String(score1)
+// let c2 = String(score2)
+// let c3 = String(score3)
+// let c4 = String(score4)
+// let c5 = String(score5)
+// let c6 = String(score6)
+// console.table([c1,typeof (c1),c2,typeof (c2),c3,typeof (c3),c4,typeof (c4),c5,typeof (c5),c6,typeof (c6)])
+
+// ************************************** OPERATIONS *****************************************
+
+// Basic arithmetic operations : + - / * ** % 
+
+// String operations
+let s1 = "Hello "
+let s2 = "Kartik"
+// console.log(s1+s2) 
+
+// to string 
+// console.log("1" + 2);
+// to string 
+// console.log(1 + "2");
+// to string 
+// console.log("1" + 2 + 2);
+// int then to string 
+// console.log(1 + 2 + "2");
+// 1
+// console.log(+true);
+// 0
+// console.log(+"");
+// error
+// console.log(true+);
+
+// PRE-FIX and POST-FIX operation
+
+let num = 100;
+num++
+console.log(num);
+++num
+console.log(num);
